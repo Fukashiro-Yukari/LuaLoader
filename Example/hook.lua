871b10751652e8f424115016449f0d3f94a8581d
Fukashiro Yukari <fukashiroyukari@tuta.io>
2022-10-07 05:15:24 +0800

    Reupdate all files

---
+++ Example/hook.lua
@@ -0,0 +1,33 @@
+
+-- GUI rendering hook
+
+hook.Add('OnGUI','Example',function()
+    -- GUI functions can be found on https://docs.unity3d.com/ScriptReference/GUI.html
+
+    GUI.Label(Rect(10,10,100,20),'Hello World!')
+
+    GUI.color = Color.blue -- Change the color
+
+    GUI.Label(Rect(10,30,100,20),'Hello World!')
+
+    GUI.color = Color.white
+
+    if GUI.Button(Rect(10,50,100,20),'Hello World!') then
+        print('Do something')
+    end
+end)
+
+-- Update hook
+
+hook.Add('OnUpdate','Example',function()
+    -- If f1 is pressed, it will trigger
+    if InputManager.GetKeyDown(KeyCode.F1) then
+        -- Do something
+    end
+end)
+
+-- Call when the level is loaded
+
+hook.Add('OnLevelWasLoaded','Example',function(level)
+    print('level is '..level)
+end)
